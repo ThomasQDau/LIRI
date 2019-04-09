a511bcffb7502af2693b56f6978672028ea03255
Thomas Dau <ThomasQDau@gmail.com>
2019-04-09 16:14:59 -0700

    m

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # LIRI
+
+## Overview
+Make a node application that takes in specific inputs and pulls information depending on the input and the parameter provided following the input.
+
+Inputs included:
+* spotify-this-song
+* concert-this
+* movie-this
+* do-what-it-says
+
+### Spotify-this-song
+If the input is **spotify-this-song** then the parameter entered afterwards should be a song title. This should do a request to Spotify and display information in the console about the song found with its Title, Artist(s), Album it is featured in, and a link to the song on Spotify.
+
+### Concert-this
+If the input is **concert-this** then the paramter entered afterwards should be an artist name. This should do a request to BandsinTown and display upcoming events with information about the venue, its location, and the date of the event in DD/MM/YYYY format.
+
+### Movie-this
+If the input is **movie-this** then the paramter entered afterwards should be a movie title. This should do a request to omdb and display information to the console including the movie's title, plot, year of release, it's imdb and Rotten Tomatoes rating (if it exist), and the actors starred in the movie.
+
+### Do-what-it-says
+If the input is **do-what-it-says** then it will go inside the file [random](random.txt) and runs the LIRI function whether is is **concert-this** or **spotify-this-song** or **movie-this** and run the respective function with the paramter provided after the input within the same file.
+
+## Video Example
+[Youtube of my working Application!](youtube.com)
--- liri.js
+++ liri.js
@@ -1,3 +1,4 @@
+//All dat node stuff
 require("dotenv").config();
 var keys = require("./keys.js");
 var fs = require("fs");
@@ -6,33 +7,42 @@ var Spotify = require('node-spotify-api');
 var spotify = new Spotify(keys.spotify);
 
 var command = process.argv[2];
-var input = '';
-for (var i = 3; i < process.argv.length; i++) {
-    if (input === '') {
-        input = process.argv[3];
-    } else {
-        input = input + ' ' + process.argv[i];
-    }
-}
+// var input = '';
+// for (var i = 3; i < process.argv.length; i++) {
+//     if (input === '') {
+//         input = process.argv[3];
+//     } else {
+//         input = input + ' ' + process.argv[i];
+//     }
+// }
 
+//The for loop above this does the same thing as what is below this
+var input = process.argv.slice(3).join(' ');
+
+// slice(2).join(' ');
 
 function concertThis() {
     // console.log('Concert');
+    if (input == '') {
+        input = 'Arianna Grande';
+    }
     var queryUrl = "https://rest.bandsintown.com/artists/" + input + "/events?app_id=codingbootcamp";
     axios.get(queryUrl).then(
         function (response) {
             // console.log(response.data);
             if (response.data.length === 0) {
-                console.log('No events can be found for this band!')
+                console.log('No events can be found for this artist! Me sorry!')
             } else {
                 for (var i = 0; i < response.data.length; i++) {
+                    console.log('===============================')
                     var data = response.data[i];
                     console.log('Venue: ' + data.venue.name);
-                    console.log('Location is at ' + data.venue.city + ' ' + data.venue.region + data.venue.country);
+                    console.log('Location is at ' + data.venue.city + ' ' + data.venue.region + ' ' + data.venue.country);
                     var dateArr = data.datetime.split('T');
                     var ddmmyyArr = dateArr[0].split('-');
                     // console.log(ddmmyyArr);
                     console.log('The event is taking place on ' + ddmmyyArr[1] + '-' + ddmmyyArr[2] + '-' + ddmmyyArr[0]);
+                    console.log('===============================')
                     // console.log(dateArr);
                 }
             }
@@ -43,11 +53,15 @@ function concertThis() {
 }
 
 function spotifyThis() {
+    if (input == '') {
+        input = 'The Sign by Ace of Base';
+    }
     // console.log('Spotify');
     spotify.search({ limit: 1, type: 'track', query: input }, function (err, data) {
         if (err) {
             return console.log('Error occurred: ' + err);
         }
+        console.log('===============================')
         // console.log(JSON.stringify(data, null, 2));
         console.log('Track Name: ' + data.tracks.items[0].name);
         console.log('Artist(s):');
@@ -56,25 +70,36 @@ function spotifyThis() {
         }
         console.log('Link: ' + data.tracks.items[0].external_urls.spotify);
         console.log('Album: ' + data.tracks.items[0].album.name);
+        console.log('===============================')
     });
 }
 
 function movieThis() {
     // console.log('Movie');
+    if (input == '') {
+        input = 'Shutter Island';
+    }
     var queryUrl = "http://www.omdbapi.com/?t=" + input + "&y=&plot=short&apikey=trilogy";
     axios.get(queryUrl).then(
         function (response) {
-            console.log(response.data.Ratings);
+            // console.log(response.data.Ratings);
+            console.log('===============================')
             console.log('Title: ' + response.data.Title);
             console.log('Release Year: ' + response.data.Year);
             console.log('IMDB rating: ' + response.data.imdbRating);
-            if (response.data.Ratings[1]) {
+            // if ('Source' in response.data.Ratings[1]) {
+            //     console.log('It exist!')
+            // }
+            if (response.data.Ratings && response.data.Ratings[1] && response.data.Ratings[1].Source === 'Rotten Tomatoes') {
                 console.log('Rotten Tomatoes rating: ' + response.data.Ratings[1].Value);
+            } else {
+                console.log('Rotten Tomatoes rating does not exist for some reason. Don\'t ask me why!');
             }
             console.log('Produced in: ' + response.data.Country);
             console.log('Language: ' + response.data.Language);
             console.log('Plot: ' + response.data.Plot);
             console.log('Actors: ' + response.data.Actors);
+            console.log('===============================')
         }
     );
 }
@@ -99,7 +124,7 @@ function doWhatItSays() {
                 movieThis();
                 break;
             case 'do-what-it-says':
-                // doWhatItSays();
+                // doWhatItSays(); //this makes it loop FOREVER!
                 console.log('We are looping forever!');
                 break;
             default:
